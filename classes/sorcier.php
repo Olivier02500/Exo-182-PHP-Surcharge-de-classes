1491770b1971d10e25b70321a8e7bd5bc76a7876
Olivier02500 <olivier.peuchet@hotmail.com>
2022-02-03 11:40:06 +0100

    make exo 182

---
+++ classes/sorcier.php
@@ -0,0 +1,43 @@
+<?php
+
+class sorcier extends Personnage {
+
+    public function __construct() {
+        parent::__construct();
+        $this->x = 125;
+        $this->y = 125;
+    }
+
+    /**
+     * @return int
+     */
+    public function getX(): int
+    {
+        return $this->x;
+    }
+
+    /**
+     * @param int $x
+     */
+    public function setX(int $x): void
+    {
+        $this->x = $x;
+    }
+
+    /**
+     * @return int
+     */
+    public function getY(): int
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param int $y
+     */
+    public function setY(int $y): void
+    {
+        $this->y = $y;
+    }
+
+}
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 require "classes/Personnage.php";
 require "classes/Dragon.php";
 require "classes/Princesse.php";
+require "classes/sorcier.php";
 
 // Creation d'une instance de la classe personnage
 $humain = new Personnage();
@@ -14,6 +15,21 @@ $humain->setNom("Votre nom");
 // Creation d'une instance de la classe dragon
 $dragon = new Dragon();
 $dragon->setNom("");
+
+$sorcier  = new sorcier();
+$sorcier->setNom('Merlin');
+echo $sorcier->getNom();
+echo "<p>". $sorcier->x . "</p>";
+echo "<p>" . $sorcier->y . "</p>";
+
+
+$princesse = new Princesse();
+$princesse->setNom('princesse Fiona');
+echo $princesse->getNom();
+echo "<p>". $princesse->x . "</p>";
+echo "<p>" . $princesse->y . "</p>";
+
+
 
 
 
